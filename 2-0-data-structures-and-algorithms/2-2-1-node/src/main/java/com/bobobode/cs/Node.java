2ee56bb902c7f06506a88a071bed6a0fef11cf49
Yurii Zaluskyi <yurazaluskyi@gmail.com>
2021-09-07 15:09:09 +0300

    commit

--- 2-0-data-structures-and-algorithms/2-2-1-node/src/main/java/com/bobobode/cs/Node.java
+++ 2-0-data-structures-and-algorithms/2-2-1-node/src/main/java/com/bobobode/cs/Node.java
@@ -1,12 +1,20 @@
 package com.bobobode.cs;
 
 /**
- * Class {@link Node} is a very simple data structure that consists of an element itself and the reference to the next
- * node. An element can have any value since it's a generic. A reference to the next node allows to link {@link Node}
- * objects and build more comprehensive data structures on top of those liked nodes.
+ * Class {@link Node} is a very simple data structure that consists of an element itself and the
+ * reference to the next node. An element can have any value since it's a generic. A reference to
+ * the next node allows to link {@link Node} objects and build more comprehensive data structures on
+ * top of those liked nodes.
  *
  * @param <T> a generic type T
  */
 public class Node<T> {
-    // todo:
+
+  // todo:
+  T element;
+  Node<T> next;
+
+  public Node(T element) {
+    this.element = element;
+  }
 }
--- 2-0-data-structures-and-algorithms/2-2-1-node/src/main/java/com/bobobode/cs/Nodes.java
+++ 2-0-data-structures-and-algorithms/2-2-1-node/src/main/java/com/bobobode/cs/Nodes.java
@@ -6,80 +6,111 @@ import com.bobocode.util.ExerciseNotCompletedException;
  * A class that consists of static methods only and provides util methods for {@link Node}.
  */
 public class Nodes {
-    private Nodes() {
-    }
 
-    /**
-     * Creates a new instance of {@link Node} that holds provided element
-     *
-     * @param element any element of type T
-     * @param <T>     generic type
-     * @return a new instance of {@link Node}
-     */
-    public static <T> Node<T> create(T element) {
-        throw new ExerciseNotCompletedException(); // todo:
-    }
+  private Nodes() {
+  }
 
-    /**
-     * Create a connection between first and second nodes, so object first stores a reference to the second.
-     *
-     * @param first  any {@link Node} object
-     * @param second any {@link Node} object
-     * @param <T>    a genetic type
-     */
-    public static <T> void link(Node<T> first, Node<T> second) {
-        throw new ExerciseNotCompletedException(); // todo:
-    }
+  /**
+   * Creates a new instance of {@link Node} that holds provided element
+   *
+   * @param element any element of type T
+   * @param <T> generic type
+   * @return a new instance of {@link Node}
+   */
+  public static <T> Node<T> create(T element) {
+//        throw new ExerciseNotCompletedException(); // todo:
+    return new Node<>(element);
+  }
 
-    /**
-     * Creates two new {@link Node} objects using provided firstElement and secondElement, and create a connection
-     * between those two elements so the first node will hold a reference to a second one.
-     *
-     * @param firstElement  any element of type T
-     * @param secondElement any element of type T
-     * @param <T>           a genetic type
-     * @return a reference to a first node created based on firstElement
-     */
-    public static <T> Node<T> pairOf(T firstElement, T secondElement) {
-        throw new ExerciseNotCompletedException(); // todo:
-    }
+  /**
+   * Create a connection between first and second nodes, so object first stores a reference to the
+   * second.
+   *
+   * @param first any {@link Node} object
+   * @param second any {@link Node} object
+   * @param <T> a genetic type
+   */
+  public static <T> void link(Node<T> first, Node<T> second) {
+//    throw new ExerciseNotCompletedException(); // todo:
+    first.next = second;
+  }
 
-    /**
-     * Creates two new {@link Node} objects using provided firstElement and secondElement, and creates connections
-     * between those nodes so the first node will hold a reference to a second one, and a second node will hold
-     * a reference to the first one.
-     *
-     * @param firstElement  any element of type T
-     * @param secondElement any element of type T
-     * @param <T>           generic type T
-     * @return a reference to the first node
-     */
-    public static <T> Node<T> closedPairOf(T firstElement, T secondElement) {
-        throw new ExerciseNotCompletedException(); // todo:
-    }
+  /**
+   * Creates two new {@link Node} objects using provided firstElement and secondElement, and create
+   * a connection between those two elements so the first node will hold a reference to a second
+   * one.
+   *
+   * @param firstElement any element of type T
+   * @param secondElement any element of type T
+   * @param <T> a genetic type
+   * @return a reference to a first node created based on firstElement
+   */
+  public static <T> Node<T> pairOf(T firstElement, T secondElement) {
+//    throw new ExerciseNotCompletedException(); // todo:
+    Node<T> first = new Node<>(firstElement);
+    first.next = new Node<>(secondElement);
+    return first;
+  }
 
-    /**
-     * Creates a linked chain of {@link Node} objects based on provided elements. Creates a connection between those
-     * nodes so each node will hold a reference to the next one in the chain. HINT: it's basically a linked list.
-     *
-     * @param elements a array of elements of type T
-     * @param <T>      generic type T
-     * @return a reference to the first element of the chain
-     */
-    public static <T> Node<T> chainOf(T... elements) {
-        throw new ExerciseNotCompletedException(); // todo:
-    }
+  /**
+   * Creates two new {@link Node} objects using provided firstElement and secondElement, and creates
+   * connections between those nodes so the first node will hold a reference to a second one, and a
+   * second node will hold a reference to the first one.
+   *
+   * @param firstElement any element of type T
+   * @param secondElement any element of type T
+   * @param <T> generic type T
+   * @return a reference to the first node
+   */
+  public static <T> Node<T> closedPairOf(T firstElement, T secondElement) {
+//    throw new ExerciseNotCompletedException(); // todo:
+    Node<T> first = new Node<>(firstElement);
+    Node<T> second = new Node<>(secondElement);
+    first.next = second;
+    second.next = first;
+    return first;
+  }
 
-    /**
-     * Creates a linked circle of {@link Node} objects based on provided elements. Creates a connection between those
-     * nodes so each node will hold a reference to the next one in the chain, and the last one will hold a reference to
-     * the first one.
-     *
-     * @param elements a array of elements of type T
-     * @param <T>      generic type T
-     * @return a reference to the first element of the chain
-     */
-    public static <T> Node<T> circleOf(T... elements) {
-        throw new ExerciseNotCompletedException(); // todo:
+  /**
+   * Creates a linked chain of {@link Node} objects based on provided elements. Creates a connection
+   * between those nodes so each node will hold a reference to the next one in the chain. HINT: it's
+   * basically a linked list.
+   *
+   * @param elements a array of elements of type T
+   * @param <T> generic type T
+   * @return a reference to the first element of the chain
+   */
+  public static <T> Node<T> chainOf(T... elements) {
+//    throw new ExerciseNotCompletedException(); // todo:
+    int n = elements.length;
+    Node<T> first = new Node<>(elements[0]);
+    Node<T> current = first;
+    for (int i = 1; i < n; i++) {
+      current.next = new Node<>(elements[i]);
+      current = current.next;
     }
+    return first;
+  }
+
+  /**
+   * Creates a linked circle of {@link Node} objects based on provided elements. Creates a
+   * connection between those nodes so each node will hold a reference to the next one in the chain,
+   * and the last one will hold a reference to the first one.
+   *
+   * @param elements a array of elements of type T
+   * @param <T> generic type T
+   * @return a reference to the first element of the chain
+   */
+  public static <T> Node<T> circleOf(T... elements) {
+//    throw new ExerciseNotCompletedException(); // todo:
+    int n = elements.length;
+    Node<T> first = new Node<>(elements[0]);
+    Node<T> current = first;
+    for (int i = 1; i < n; i++) {
+      current.next = new Node<>(elements[i]);
+      current = current.next;
+    }
+    current.next = first;
+    return first;
+  }
 }
