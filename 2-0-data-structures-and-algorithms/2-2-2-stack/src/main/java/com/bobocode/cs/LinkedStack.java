8e48fca0c029f667d40289c975f08388a816b24c
Yurii Zaluskyi <yurazaluskyi@gmail.com>
2021-09-07 15:35:50 +0300

    commit

--- 2-0-data-structures-and-algorithms/2-2-2-stack/src/main/java/com/bobocode/cs/LinkedStack.java
+++ 2-0-data-structures-and-algorithms/2-2-2-stack/src/main/java/com/bobocode/cs/LinkedStack.java
@@ -2,67 +2,105 @@ package com.bobocode.cs;
 
 import com.bobocode.cs.exception.EmptyStackException;
 import com.bobocode.util.ExerciseNotCompletedException;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 /**
- * {@link LinkedStack} is a stack implementation that is based on singly linked generic nodes.
- * A node is implemented as inner static class {@link Node<T>}.
+ * {@link LinkedStack} is a stack implementation that is based on singly linked generic nodes. A
+ * node is implemented as inner static class {@link Node<T>}.
  *
  * @param <T> generic type parameter
  */
 public class LinkedStack<T> implements Stack<T> {
 
-    /**
-     * This method creates a stack of provided elements
-     *
-     * @param elements elements to add
-     * @param <T>      generic type
-     * @return a new stack of elements that were passed as method parameters
-     */
-    public static <T> LinkedStack<T> of(T... elements) {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
+  Node<T> head;
+  int size;
 
-    /**
-     * The method pushes an element onto the top of this stack. This has exactly the same effect as:
-     * addElement(item)
-     *
-     * @param element elements to add
-     */
-    @Override
-    public void push(T element) {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
+  private static class Node<T> {
 
-    /**
-     * This method removes the object at the top of this stack
-     * and returns that object as the value of this function.
-     *
-     * @return The object at the top of this stack
-     * @throws EmptyStackException - if this stack is empty
-     */
-    @Override
-    public T pop() {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
+    T element;
+    Node<T> next;
 
-    /**
-     * Returns the number of elements in the stack
-     *
-     * @return number of elements
-     */
-    @Override
-    public int size() {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
+    public Node(T element) {
+      this.element = element;
     }
+  }
 
-    /**
-     * Checks if a stack is empty
-     *
-     * @return {@code true} if a stack is empty, {@code false} otherwise
-     */
-    @Override
-    public boolean isEmpty() {
-        throw new ExerciseNotCompletedException(); // todo: implement this method;
+  /**
+   * This method creates a stack of provided elements
+   *
+   * @param elements elements to add
+   * @param <T> generic type
+   * @return a new stack of elements that were passed as method parameters
+   */
+  public static <T> LinkedStack<T> of(T... elements) {
+//    throw new ExerciseNotCompletedException(); // todo: implement this method
+    LinkedStack<T> linkedStack = new LinkedStack<>();
+    Stream.of(elements).forEach(linkedStack::push);
+    linkedStack.size = elements.length;
+    return linkedStack;
+  }
+
+  /**
+   * The method pushes an element onto the top of this stack. This has exactly the same effect as:
+   * addElement(item)
+   *
+   * @param element elements to add
+   */
+  @Override
+  public void push(T element) {
+//    throw new ExerciseNotCompletedException(); // todo: implement this method
+    Objects.requireNonNull(element);
+    Node<T> node;
+    if (size == 0) {
+      head = new Node<>(element);
+    } else {
+      node = head;
+      head = new Node<>(element);
+      head.next = node;
     }
+    size++;
+  }
+
+  /**
+   * This method removes the object at the top of this stack and returns that object as the value of
+   * this function.
+   *
+   * @return The object at the top of this stack
+   * @throws EmptyStackException - if this stack is empty
+   */
+  @Override
+  public T pop() {
+//    throw new ExerciseNotCompletedException(); // todo: implement this method
+    if (size == 0) {
+      throw new EmptyStackException();
+    }
+    Node<T> node = head;
+    head = head.next;
+    size--;
+    return node.element;
+  }
+
+  /**
+   * Returns the number of elements in the stack
+   *
+   * @return number of elements
+   */
+  @Override
+  public int size() {
+//    throw new ExerciseNotCompletedException(); // todo: implement this method
+    return size;
+  }
+
+  /**
+   * Checks if a stack is empty
+   *
+   * @return {@code true} if a stack is empty, {@code false} otherwise
+   */
+  @Override
+  public boolean isEmpty() {
+//    throw new ExerciseNotCompletedException(); // todo: implement this method;
+    return size == 0;
+  }
 
 }
