4dba9ebaf818d2ad47f75eccaaa37b9505469dd6
Yurii Zaluskyi <yurazaluskyi@gmail.com>
2021-09-09 22:15:13 +0300

    commit

--- 2-0-data-structures-and-algorithms/2-2-5-array-list/src/main/java/com/bobocode/cs/ArrayList.java
+++ 2-0-data-structures-and-algorithms/2-2-5-array-list/src/main/java/com/bobocode/cs/ArrayList.java
@@ -1,6 +1,8 @@
 package com.bobocode.cs;
 
 import com.bobocode.util.ExerciseNotCompletedException;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 /**
  * {@link ArrayList} is an implementation of {@link List} interface. This resizable data structure
@@ -8,147 +10,179 @@ import com.bobocode.util.ExerciseNotCompletedException;
  */
 public class ArrayList<T> implements List<T> {
 
-    /**
-     * This constructor creates an instance of {@link ArrayList} with a specific capacity of an array inside.
-     *
-     * @param initCapacity - the initial capacity of the list
-     * @throws IllegalArgumentException – if the specified initial capacity is negative or 0.
-     */
-    public ArrayList(int initCapacity) {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
+  private static final int DEFAULT_CAPACITY = 5;
+  private int size;
+  private Object[] array;
 
-    /**
-     * This constructor creates an instance of {@link ArrayList} with a default capacity of an array inside.
-     * A default size of inner array is 5;
-     */
-    public ArrayList() {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
+  /**
+   * This constructor creates an instance of {@link ArrayList} with a specific capacity of an array
+   * inside.
+   *
+   * @param initCapacity - the initial capacity of the list
+   * @throws IllegalArgumentException – if the specified initial capacity is negative or 0.
+   */
+  public ArrayList(int initCapacity) {
+//    throw new ExerciseNotCompletedException(); // todo: implement this method
+    if (initCapacity < 0) {
+      throw new IllegalArgumentException();
     }
+    array = new Object[initCapacity];
+  }
 
-    /**
-     * Creates and returns an instance of {@link ArrayList} with provided elements
-     *
-     * @param elements to add
-     * @return new instance
-     */
-    public static <T> List<T> of(T... elements) {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
+  /**
+   * This constructor creates an instance of {@link ArrayList} with a default capacity of an array
+   * inside. A default size of inner array is 5;
+   */
+  public ArrayList() {
+//    throw new ExerciseNotCompletedException(); // todo: implement this method
+    array = new Object[DEFAULT_CAPACITY];
+  }
 
-    /**
-     * Adds an element to the array.
-     *
-     * @param element element to add
-     */
-    @Override
-    public void add(T element) {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
+  /**
+   * Creates and returns an instance of {@link ArrayList} with provided elements
+   *
+   * @param elements to add
+   * @return new instance
+   */
+  public static <T> List<T> of(T... elements) {
+    throw new ExerciseNotCompletedException(); // todo: implement this method
+  }
 
-    /**
-     * Adds an element to the specific position in the array where
-     *
-     * @param index   index of position
-     * @param element element to add
-     */
-    @Override
-    public void add(int index, T element) {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
+  /**
+   * Adds an element to the array.
+   *
+   * @param element element to add
+   */
+  @Override
+  public void add(T element) {
+    throw new ExerciseNotCompletedException(); // todo: implement this method
+  }
 
-    /**
-     * Retrieves an element by its position index. In case provided index in out of the list bounds it
-     * throws {@link IndexOutOfBoundsException}
-     *
-     * @param index index of element
-     * @return en element
-     */
-    @Override
-    public T get(int index) {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
+  /**
+   * Adds an element to the specific position in the array where
+   *
+   * @param index index of position
+   * @param element element to add
+   */
+  @Override
+  public void add(int index, T element) {
+    throw new ExerciseNotCompletedException(); // todo: implement this method
+  }
 
-    /**
-     * Returns the first element of the list. Operation is performed in constant time O(1)
-     *
-     * @return the first element of the list
-     * @throws java.util.NoSuchElementException if list is empty
-     */
-    @Override
-    public T getFirst() {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
+  /**
+   * Retrieves an element by its position index. In case provided index in out of the list bounds it
+   * throws {@link IndexOutOfBoundsException}
+   *
+   * @param index index of element
+   * @return en element
+   */
+  @Override
+  public T get(int index) {
+//    throw new ExerciseNotCompletedException(); // todo: implement this method
+    Objects.checkIndex(index, size);
+    return (T) array[index];
+  }
 
-    /**
-     * Returns the last element of the list. Operation is performed in constant time O(1)
-     *
-     * @return the last element of the list
-     * @throws java.util.NoSuchElementException if list is empty
-     */
-    @Override
-    public T getLast() {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
+  /**
+   * Returns the first element of the list. Operation is performed in constant time O(1)
+   *
+   * @return the first element of the list
+   * @throws java.util.NoSuchElementException if list is empty
+   */
+  @Override
+  public T getFirst() {
+//    throw new ExerciseNotCompletedException(); // todo: implement this method
+    if (size == 0) {
+      throw new NoSuchElementException();
     }
+    return (T) array[0];
+  }
 
-    /**
-     * Changes the value of array at specific position. In case provided index in out of the list bounds it
-     * throws {@link IndexOutOfBoundsException}
-     *
-     * @param index   position of value
-     * @param element a new value
-     */
-    @Override
-    public void set(int index, T element) {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
+  /**
+   * Returns the last element of the list. Operation is performed in constant time O(1)
+   *
+   * @return the last element of the list
+   * @throws java.util.NoSuchElementException if list is empty
+   */
+  @Override
+  public T getLast() {
+//    throw new ExerciseNotCompletedException(); // todo: implement this method
+    if (size == 0) {
+      throw new NoSuchElementException();
     }
+    return (T) array[size - 1];
+  }
 
-    /**
-     * Removes an elements by its position index. In case provided index in out of the list bounds it
-     * throws {@link IndexOutOfBoundsException}
-     *
-     * @param index element index
-     * @return deleted element
-     */
-    @Override
-    public T remove(int index) {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
+  /**
+   * Changes the value of array at specific position. In case provided index in out of the list
+   * bounds it throws {@link IndexOutOfBoundsException}
+   *
+   * @param index position of value
+   * @param element a new value
+   */
+  @Override
+  public void set(int index, T element) {
+//    throw new ExerciseNotCompletedException(); // todo: implement this method
+    Objects.checkIndex(index, size);
+    array[index] = element;
+  }
 
-    /**
-     * Checks for existing of a specific element in the list.
-     *
-     * @param element is element
-     * @return If element exists method returns true, otherwise it returns false
-     */
-    @Override
-    public boolean contains(T element) {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
+  /**
+   * Removes an elements by its position index. In case provided index in out of the list bounds it
+   * throws {@link IndexOutOfBoundsException}
+   *
+   * @param index element index
+   * @return deleted element
+   */
+  @Override
+  public T remove(int index) {
+    throw new ExerciseNotCompletedException(); // todo: implement this method
+  }
 
-    /**
-     * Checks if a list is empty
-     *
-     * @return {@code true} if list is empty, {@code false} otherwise
-     */
-    @Override
-    public boolean isEmpty() {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
+  /**
+   * Checks for existing of a specific element in the list.
+   *
+   * @param element is element
+   * @return If element exists method returns true, otherwise it returns false
+   */
+  @Override
+  public boolean contains(T element) {
+//    throw new ExerciseNotCompletedException(); // todo: implement this method
+    for (int i = 0; i < size; i++) {
+      if (array[i].equals(element)) {
+        return true;
+      }
     }
+    return false;
+  }
 
-    /**
-     * @return amount of saved elements
-     */
-    @Override
-    public int size() {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
+  /**
+   * Checks if a list is empty
+   *
+   * @return {@code true} if list is empty, {@code false} otherwise
+   */
+  @Override
+  public boolean isEmpty() {
+//    throw new ExerciseNotCompletedException(); // todo: implement this method
+    return size == 0;
+  }
 
-    /**
-     * Removes all list elements
-     */
-    @Override
-    public void clear() {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
+  /**
+   * @return amount of saved elements
+   */
+  @Override
+  public int size() {
+//    throw new ExerciseNotCompletedException(); // todo: implement this method
+    return size;
+  }
+
+  /**
+   * Removes all list elements
+   */
+  @Override
+  public void clear() {
+//    throw new ExerciseNotCompletedException(); // todo: implement this method
+    size = 0;
+    array = new Object[DEFAULT_CAPACITY];
+  }
 }
