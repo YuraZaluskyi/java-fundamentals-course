fa5fbd22ed9bff5492b31df70ee717fa5eb626ce
Yurii Zaluskyi <yurazaluskyi@gmail.com>
2021-09-19 10:09:59 +0300

    commit

---
+++ 2-0-data-structures-and-algorithms/2-2-7-sort-algorithms/src/main/java/com/bobcode/cs/MergeSort.java
@@ -0,0 +1,53 @@
+package com.bobcode.cs;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+  private static int[] array = {3, 6, 1, 9, 4, 2, 0};
+
+  public static void main(String[] args) {
+    System.out.println(Arrays.toString(array));
+    mergeSort(array, array.length);
+    System.out.println(Arrays.toString(array));
+
+  }
+
+  public static void mergeSort(int[] array, int n) {
+    if (n < 2) {
+      return;
+    }
+    int mid = n / 2;
+    int[] l = new int[mid];
+    int[] r = new int[n - mid];
+    for (int i = 0; i < mid; i++) {
+      l[i] = array[i];
+    }
+    for (int i = mid; i < n; i++) {
+      r[i - mid] = array[i];
+    }
+    mergeSort(l, mid);
+    mergeSort(r, n - mid);
+
+    merge(array, l, r, mid, n - mid);
+  }
+
+  private static void merge(int[] a, int[] l, int[] r, int left, int right) {
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    while (i < left && j < right) {
+      if (l[i] <= r[j]) {
+        a[k++] = l[i++];
+      } else {
+        a[k++] = r[j++];
+      }
+    }
+    while (i < left) {
+      a[k++] = l[i++];
+    }
+    while (j < right) {
+      a[k++] = r[j++];
+    }
+  }
+}
--- 2-0-data-structures-and-algorithms/2-2-7-sort-algorithms/src/test/java/com/bobcode/cs/MergeSortTest.java
+++ 2-0-data-structures-and-algorithms/2-2-7-sort-algorithms/src/test/java/com/bobcode/cs/MergeSortTest.java
@@ -3,13 +3,14 @@ package com.bobcode.cs;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class InsertionSortTest {
+class MergeSortTest {
+
   private static int[] array = {3, 6, 1, 9, 4, 2, 0};
   private static int[] expectedArray = {0, 1, 2, 3, 4, 6, 9};
 
   @Test
-  void insertSort() {
+  void mergeSort() {
     InsertionSort.insertSort(array);
-    Assertions.assertArrayEquals(expectedArray,array);
+    Assertions.assertArrayEquals(expectedArray, array);
   }
 }
